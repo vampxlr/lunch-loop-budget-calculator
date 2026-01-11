4fde7ec77f693da813391341bcd8de4a9db7efb7
Kamrul Ahsan <vampxlr@yahoo.com>
2026-01-11 20:18:13 +0600

    fix build errors vercel part 2

--- components/ui/slider.tsx
+++ components/ui/slider.tsx
@@ -2,7 +2,7 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface SliderProps
-  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "onChange"> {
+  extends Omit<React.InputHTMLAttributes<HTMLInputElement>, "type" | "onChange" | "value"> {
   value?: number[];
   onValueChange?: (value: number[]) => void;
 }
